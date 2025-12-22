823be3fce65c8be03c019b82702fdd5146c1016f
Bilgehan Eybek <b.eybek9307@gmail.com>
2025-12-22 20:16:27 +0300

    logo updated

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,10 @@
 export default function Logo() {
   return (
-    <div className="flex space-x-5">
+    <div className="flex space-x-3 sm:space-x-5">
       <img src="/assets/html-logo.png" className="w-15 h-15 lg:w-20 lg:h-24" />
 
-      <div className="flex  justify-center items-center">
-        <h1 className=" font-serif">Modern & Legacy UI Developer </h1>
+      <div className="flex justify-center items-center">
+        <h1 className="font-sm sm:font-md font-serif">Modern & Legacy UI Developer </h1>
       </div>
     </div>
   );
