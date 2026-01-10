96e47f40f826ebc17fdd6dfc648855a7fe38a413
Bilgehan Eybek <b.eybek9307@gmail.com>
2026-01-10 23:47:02 +0300

    ff

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,10 +9,8 @@ type HeroProps = {
 export function Hero({ onScrollClick }: HeroProps) {
   return (
     <div className="sm:flex-reverse lg:flex  h-screen w-full justify-between bg-[url('/assets/bilge.png')] bg-no-repeat px-5 lg:px-15  bg-center ">
-      <div className="flex flex-col justify-center lg:justify-start lg:gap-20  sm:w-1/2 mt-0 lg:mt-40  ">
-<div className="hidden lg:block  w-36 h-[10px]  bg-white/10 rounded-full">
-</div>
-
+      <div className="flex flex-col justify-center lg:justify-start lg:gap-20  sm:w-1/2 mt-0 lg:mt-50  ">
+       <div className="hidden lg:block  w-36 h-[10px]  bg-white/10 rounded-full"></div>
         <div className="hidden lg:block space-y-5 md:space-y-10">
           <h1 className="text-3xl bg-black/60 lg:bg-transparent md:text-3xl text-amber-100 font-sans sm:font-mono sm:tracking-wider">
             I&apos;m Bilgehan
@@ -40,7 +38,7 @@ export function Hero({ onScrollClick }: HeroProps) {
         </div>
       </div>
 
-      <div className="hidden md:flex flex-col w-1/2 md:w-1/3 mt-20 lg:mt-0 lg:mb-90  space-y-5 xl:space-y-10 text-xs">
+      <div className="hidden md:flex flex-col w-1/2 md:w-1/3  lg:mt-0 lg:mb-90  space-y-5 xl:space-y-10 text-xs">
 
 
    <div className="relative w-full h-full flex items-center justify-center">
@@ -50,20 +48,20 @@ export function Hero({ onScrollClick }: HeroProps) {
   <div className="relative z-10 w-80 h-40 rounded-xl bg-black/30 backdrop-blur-md border border-white/10 
                 flex flex-col justify-center gap-3 p-4 text-[11px]">
 
-  <span className="inline-block w-max px-3 py-1 rounded-full 
-                   bg-white/5 border border-white/10 
+  <span className=" inline-block w-max px-3 py-1 rounded-full 
+                   bg-white/5 border border-white/10 text-sm
                    text-purple-300 tracking-wide">
     Scalable UI Systems
   </span>
 
   <span className="inline-block w-max px-3 py-1 rounded-full 
-                   bg-white/5 border border-white/10 
+                   bg-white/5 border border-white/10    text-sm
                    text-blue-300 tracking-wide">
     Performance-Focused Design
   </span>
 
   <span className="inline-block w-max px-3 py-1 rounded-full 
-                   bg-white/5 border border-white/10 
+                   bg-white/5 border border-white/10   text-sm
                    text-gray-200 tracking-wide">
     Clean & Maintainable Code
   </span>
