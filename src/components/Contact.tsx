dde0980e8adc986bddef76a542fdf71f608b2627
Bilgehan Eybek <b.eybek9307@gmail.com>
2026-02-06 14:37:43 +0300

    ff

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@ import ContactForm from "./ContactForm";
 
 export default function Contact() {
   return (
-    <div className="w-full h-dvh  portrait:py-48  flex justify-center items-center  p-5 lg:px-20">
+    <div className="w-full h-dvh  portrait:py-48 portrait:pb-120 flex justify-center items-center  p-5 lg:px-20">
       <ContactForm />
     </div>
   );
